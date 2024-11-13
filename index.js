47913ca9a74480859026cc599633fad54df02635
sabha mushtaq <sabhamushtaq191@gmail.com>
2024-11-13 19:31:28 +0530

    changed address of views and public folder

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const path = require('path');
 const ejs = require('ejs');
 const app = express();
 app.set('view engine','ejs')
-app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'mainfolder', 'views'));
+
+app.use(express.static(path.join(__dirname, 'mainfolder', 'public')));
+
 
 
 const session = require('express-session');
